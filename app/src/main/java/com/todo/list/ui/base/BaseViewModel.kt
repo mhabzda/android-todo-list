cb2bed6d5fbb1b9e7f51fee12542208748e1858e
Marcin.Habzda <marcin.habzda@miquido.com>
2022-02-24 10:45:37 +0100

    Create BaseViewModel

---
+++ app/src/main/java/com/todo/list/ui/base/BaseViewModel.kt
@@ -0,0 +1,23 @@
+package com.todo.list.ui.base
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+
+abstract class BaseViewModel<State, Event>(defaultState: State) : ViewModel() {
+    private val stateFlow = MutableStateFlow(defaultState)
+    val state = stateFlow.asStateFlow()
+
+    private val eventChannel = Channel<Event>(Channel.BUFFERED)
+    val events = eventChannel.receiveAsFlow()
+
+    protected fun updateState(block: State.() -> State) {
+        stateFlow.value = block(stateFlow.value)
+    }
+
+    protected suspend fun sendEvent(action: Event) {
+        eventChannel.send(action)
+    }
+}
--- app/src/main/java/com/todo/list/ui/list/ListViewModel.kt
+++ app/src/main/java/com/todo/list/ui/list/ListViewModel.kt
@@ -1,11 +1,11 @@
 package com.todo.list.ui.list
 
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import androidx.paging.CombinedLoadStates
 import androidx.paging.LoadState
 import com.todo.list.model.entities.TodoItem
 import com.todo.list.model.repository.TodoRepository
+import com.todo.list.ui.base.BaseViewModel
 import com.todo.list.ui.list.data.ListViewEvent
 import com.todo.list.ui.list.data.ListViewEvent.Error
 import com.todo.list.ui.list.data.ListViewState
@@ -13,12 +13,8 @@ import com.todo.list.ui.list.navigation.ListRouter
 import com.todo.list.ui.parcel.TodoItemToParcelableMapper
 import com.todo.list.utils.EMPTY
 import com.todo.list.utils.onTerminate
-import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -26,15 +22,9 @@ class ListViewModel @Inject constructor(
     private val todoRepository: TodoRepository,
     private val router: ListRouter,
     private val todoItemToParcelableMapper: TodoItemToParcelableMapper
-) : ViewModel() {
+) : BaseViewModel<ListViewState, ListViewEvent>(ListViewState()) {
 
     val pagingEvents = todoRepository.fetchTodoItems(PAGE_SIZE)
-
-    private val eventChannel = Channel<ListViewEvent>(Channel.BUFFERED)
-    val events = eventChannel.receiveAsFlow()
-
-    private val stateFlow = MutableStateFlow(ListViewState())
-    val state = stateFlow.asStateFlow()
 
     fun onCreate(loadStateFlow: Flow<CombinedLoadStates>) {
         viewModelScope.launch {
@@ -45,14 +35,12 @@ class ListViewModel @Inject constructor(
     }
 
     private suspend fun handleLoadState(state: CombinedLoadStates) {
-        stateFlow.value = stateFlow.value.copy(
-            isRefreshing = state.refresh is LoadState.Loading || state.append is LoadState.Loading
-        )
+        updateState { copy(isRefreshing = state.refresh is LoadState.Loading || state.append is LoadState.Loading) }
 
         val refreshState = state.refresh
         val appendState = state.append
-        if (refreshState is LoadState.Error) eventChannel.send(Error(refreshState.error.message ?: ""))
-        if (appendState is LoadState.Error) eventChannel.send(Error(appendState.error.message ?: ""))
+        if (refreshState is LoadState.Error) sendEvent(Error(refreshState.error.message ?: ""))
+        if (appendState is LoadState.Error) sendEvent(Error(appendState.error.message ?: ""))
     }
 
     private fun observeItemsChanges() = viewModelScope.launch {
@@ -62,7 +50,7 @@ class ListViewModel @Inject constructor(
     }
 
     fun onItemsRefresh() = viewModelScope.launch {
-        eventChannel.send(ListViewEvent.RefreshItems)
+        sendEvent(ListViewEvent.RefreshItems)
     }
 
     fun onFloatingButtonClick() {
@@ -76,11 +64,11 @@ class ListViewModel @Inject constructor(
     }
 
     private fun deleteItem(item: TodoItem) = viewModelScope.launch {
-        stateFlow.value = stateFlow.value.copy(isRefreshing = true)
+        updateState { copy(isRefreshing = true) }
         todoRepository.deleteItem(item)
-            .onSuccess { eventChannel.send(ListViewEvent.DisplayDeletionConfirmation) }
-            .onFailure { eventChannel.send(Error(it.message ?: EMPTY)) }
-            .onTerminate { stateFlow.value = stateFlow.value.copy(isRefreshing = false) }
+            .onSuccess { sendEvent(ListViewEvent.DisplayDeletionConfirmation) }
+            .onFailure { sendEvent(Error(it.message ?: EMPTY)) }
+            .onTerminate { updateState { copy(isRefreshing = false) } }
     }
 
     fun onItemClick(item: TodoItem) {
