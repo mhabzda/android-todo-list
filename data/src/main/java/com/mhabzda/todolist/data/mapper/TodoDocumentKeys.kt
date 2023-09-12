df284c2501af0bbdbd062a375546dbe9a319c078
mhabzda <marcin.habzda1@gmail.com>
2023-09-12 16:45:16 +0200

    Use ZonedDateTime

--- data/src/main/java/com/mhabzda/todolist/data/mapper/TodoDocumentKeys.kt
+++ data/src/main/java/com/mhabzda/todolist/data/mapper/TodoDocumentKeys.kt
@@ -3,6 +3,6 @@ package com.mhabzda.todolist.data.mapper
 internal object TodoDocumentKeys {
     const val TITLE_KEY = "title"
     const val DESCRIPTION_KEY = "description"
-    const val CREATION_DATE_KEY = "creationDate"
+    const val CREATION_DATE_TIME_KEY = "creationDate"
     const val LOGO_URL_KEY = "logoUrl"
 }
--- data/src/main/java/com/mhabzda/todolist/data/mapper/TodoDocumentMapper.kt
+++ data/src/main/java/com/mhabzda/todolist/data/mapper/TodoDocumentMapper.kt
@@ -1,7 +1,7 @@
 package com.mhabzda.todolist.data.mapper
 
 import com.google.firebase.firestore.DocumentSnapshot
-import com.mhabzda.todolist.data.mapper.TodoDocumentKeys.CREATION_DATE_KEY
+import com.mhabzda.todolist.data.mapper.TodoDocumentKeys.CREATION_DATE_TIME_KEY
 import com.mhabzda.todolist.data.mapper.TodoDocumentKeys.DESCRIPTION_KEY
 import com.mhabzda.todolist.data.mapper.TodoDocumentKeys.LOGO_URL_KEY
 import com.mhabzda.todolist.data.mapper.TodoDocumentKeys.TITLE_KEY
@@ -14,7 +14,7 @@ internal class TodoDocumentMapper @Inject constructor() {
             id = document.id,
             title = document.getString(TITLE_KEY).orEmpty(),
             description = document.getString(DESCRIPTION_KEY).orEmpty(),
-            creationDate = document.getString(CREATION_DATE_KEY).toCreationDateTime(),
+            creationDateTime = document.getString(CREATION_DATE_TIME_KEY).toCreationDateTime(),
             iconUrl = document.getString(LOGO_URL_KEY)
         )
 }
