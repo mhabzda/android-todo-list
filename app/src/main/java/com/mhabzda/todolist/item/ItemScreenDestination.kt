2fc851f3a1b84473110d85e0e553a4209097af21
mhabzda <marcin.habzda1@gmail.com>
2023-09-26 23:05:13 +0200

    Refactor destinations

--- app/src/main/java/com/mhabzda/todolist/item/ItemScreenDestination.kt
+++ app/src/main/java/com/mhabzda/todolist/item/ItemScreenDestination.kt
@@ -1,19 +1,26 @@
 package com.mhabzda.todolist.item
 
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Left
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Right
+import androidx.compose.animation.core.tween
 import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavType
+import androidx.navigation.compose.composable
 import androidx.navigation.navArgument
 import com.mhabzda.todolist.Destination
+import com.mhabzda.todolist.NAVIGATION_ANIMATION_DURATION_MILLIS
 
 data object ItemScreenDestination : Destination() {
 
     const val ITEM_ID_ARG_NAME = "itemId"
 
-    override val name: String = "item/?$ITEM_ID_ARG_NAME={$ITEM_ID_ARG_NAME}"
+    private const val route: String = "item/?$ITEM_ID_ARG_NAME={$ITEM_ID_ARG_NAME}"
 
-    override fun getDirection(vararg argument: Any?): String {
+    override fun getNavigationRoute(vararg argument: Any?): String {
         val itemId = argument.firstOrNull() as? String
-        return itemId?.let { name.replace("{$ITEM_ID_ARG_NAME}", it) } ?: name
+        return itemId?.let { route.replace("{$ITEM_ID_ARG_NAME}", it) } ?: route
     }
 
     override fun getNavArguments(): List<NamedNavArgument> =
@@ -23,4 +30,17 @@ data object ItemScreenDestination : Destination() {
                 nullable = true
             },
         )
+
+    override fun define(navGraphBuilder: NavGraphBuilder, navController: NavController) = with(navGraphBuilder) {
+        composable(
+            route = this@ItemScreenDestination.route,
+            arguments = getNavArguments(),
+            enterTransition = { slideIntoContainer(towards = Left, animationSpec = tween(NAVIGATION_ANIMATION_DURATION_MILLIS)) },
+            exitTransition = { slideOutOfContainer(towards = Right, animationSpec = tween(NAVIGATION_ANIMATION_DURATION_MILLIS)) },
+        ) {
+            ItemScreen(
+                navigateBack = navController::navigateUp
+            )
+        }
+    }
 }
--- app/src/main/java/com/mhabzda/todolist/list/ListScreenDestination.kt
+++ app/src/main/java/com/mhabzda/todolist/list/ListScreenDestination.kt
@@ -1,10 +1,34 @@
 package com.mhabzda.todolist.list
 
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Left
+import androidx.compose.animation.AnimatedContentTransitionScope.SlideDirection.Companion.Right
+import androidx.compose.animation.core.tween
 import androidx.navigation.NamedNavArgument
+import androidx.navigation.NavController
+import androidx.navigation.NavGraphBuilder
+import androidx.navigation.compose.composable
 import com.mhabzda.todolist.Destination
+import com.mhabzda.todolist.NAVIGATION_ANIMATION_DURATION_MILLIS
+import com.mhabzda.todolist.item.ItemScreenDestination
 
 data object ListScreenDestination : Destination() {
-    override val name: String = "list"
-    override fun getDirection(vararg argument: Any?) = name
+
+    const val ROUTE: String = "list"
+
+    override fun getNavigationRoute(vararg argument: Any?) = ROUTE
+
     override fun getNavArguments(): List<NamedNavArgument> = emptyList()
+
+    override fun define(navGraphBuilder: NavGraphBuilder, navController: NavController) = with(navGraphBuilder) {
+        composable(
+            route = this@ListScreenDestination.ROUTE,
+            exitTransition = { slideOutOfContainer(towards = Left, animationSpec = tween(NAVIGATION_ANIMATION_DURATION_MILLIS)) },
+            popEnterTransition = { slideIntoContainer(towards = Right, animationSpec = tween(NAVIGATION_ANIMATION_DURATION_MILLIS)) },
+        ) {
+            ListScreen(
+                navigateToCreateItem = { navController.navigate(ItemScreenDestination.getNavigationRoute()) },
+                navigateToEditItem = { navController.navigate(ItemScreenDestination.getNavigationRoute(it)) },
+            )
+        }
+    }
 }
