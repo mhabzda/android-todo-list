8fd652175455df27ad3ee35c4f8c2dfd51d7fa84
mhabzda <marcin.habzda1@gmail.com>
2023-09-27 18:11:16 +0200

    Improve compose ItemScreen

--- app/src/main/java/com/mhabzda/todolist/item/ItemContract.kt
+++ app/src/main/java/com/mhabzda/todolist/item/ItemContract.kt
@@ -6,13 +6,14 @@ import com.mhabzda.todolist.R
 interface ItemContract {
     data class ItemViewState(
         val buttonText: Int = R.string.item_creation_button_title,
-        val isLoading: Boolean = false
+        val isLoading: Boolean = false,
     )
 
     sealed class ItemEffect {
         data class InitTitle(val title: String) : ItemEffect()
         data class InitDescription(val description: String) : ItemEffect()
         data class InitIconUrl(val iconUrl: String) : ItemEffect()
+        data object DisplayTitleError : ItemEffect()
         data class DisplayMessageRes(@StringRes val messageRes: Int) : ItemEffect()
         data class DisplayMessage(val message: String) : ItemEffect()
         data object Close : ItemEffect()
